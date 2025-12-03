d150d1fb4a6cb7a8347663fd643ae946fad837be
Pixris Xie <pixris.xie@asiapac.com.cn>
2025-12-03 17:31:49 +0800

    commit

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiBase = (
-  import.meta.env.VITE_API_BASE || 'http://localhost:4000/api'
+  import.meta.env.VITE_API_BASE || 'https://pdcabinetapi.pixris.online/api'
 ).replace(/\/$/, '');
 
 const assetBase = (
